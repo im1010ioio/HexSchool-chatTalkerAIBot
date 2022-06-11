f709443e1f0f12be3c133df0d065ed103c5d79ec
Eva <im1010ioio@gmail.com>
2022-06-12 01:12:36 +0800

    加入 js 行為

---
+++ js/price.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $("#btn-top").click(function () {
+        $('html,body').animate({ scrollTop: 0 }, 'slow');
+    });
+
+    $(window).scroll(function () {
+        var scrollTop = $(this).scrollTop();
+        if (scrollTop > 10) {
+            $(".navbar").addClass("scrolling");
+        } else {
+            $(".navbar").removeClass("scrolling");
+        };
+    });
+});
