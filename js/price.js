2a4bab0cad891fcc4c06a9eb831fb78547805468
Eva <im1010ioio@gmail.com>
2022-06-12 20:35:13 +0800

    新增帶入數值 js

--- js/price.js
+++ js/price.js
@@ -1,14 +1,34 @@
 $(document).ready(function () {
-    $("#btn-top").click(function () {
-        $('html,body').animate({ scrollTop: 0 }, 'slow');
-    });
+    var currentUserCount = $("input[name=plan]:checked").val();
 
-    $(window).scroll(function () {
-        var scrollTop = $(this).scrollTop();
-        if (scrollTop > 10) {
-            $(".navbar").addClass("scrolling");
-        } else {
-            $(".navbar").removeClass("scrolling");
-        };
+    function setPrice(litePrice, proPrice) {
+        $("#text-price-lite").text(litePrice);
+        $("#text-price-pro").text(proPrice);
+    };
+
+    $(".btn-price").click(function () {
+        setTimeout(() => {
+            currentUserCount = $("input[name=plan]:checked").val();
+            console.log(currentUserCount);
+            $(".text-user-count").text(currentUserCount);
+            switch (currentUserCount) {
+                case "10000":
+                    setPrice(600, 1600);
+                    break;
+                case "15000":
+                    setPrice(900, 2000);
+                    break;
+                case "20000":
+                    setPrice(1200, 2400);
+                    break;
+                case "25000":
+                    setPrice(1800, 2800);
+                    break;
+                case ">25000":
+                    setPrice(2400, 3600);
+                    break;
+                default:
+            }
+        }, 350);
     });
 });
